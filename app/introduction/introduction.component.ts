125a9e2ea09091b18f147f3ec3106b2cd980c17a
Laury Lafage <lowrey97x@hotmail.fr>
2016-11-19 12:31:18 +0100

    Login page front-end.

--- app/introduction/introduction.component.ts
+++ app/introduction/introduction.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, AfterContentInit } from '@angular/core';
+declare var $:any;
+declare var demo:any;
 
 @Component({
   moduleId: module.id,
   selector: 'introduction',
   templateUrl: 'introduction.component.html'
 })
-export class IntroductionComponent { }
+export class IntroductionComponent implements AfterContentInit {
+
+  ngAfterContentInit() {
+    demo.checkFullPageBackgroundImage();
+
+    setTimeout(function() {
+        // after 1000 ms we add the class animated to the login/register card
+        $('.card').removeClass('card-hidden');
+    }, 700);
+  }
+}
