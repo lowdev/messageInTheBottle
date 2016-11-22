255e7f58d1a88e9bdb48220f88fe644b22d91f76
Laury Lafage <lowrey97x@hotmail.fr>
2016-11-22 23:28:25 +0100

    Design web site.

--- app/dashboard/dashboard.component.ts
+++ app/dashboard/dashboard.component.ts
@@ -3,7 +3,8 @@ import { Component, AfterContentInit, EventEmitter } from '@angular/core';
 @Component({
   moduleId: module.id,
   selector: 'dashboard',
-  templateUrl: 'dashboard.component.html'
+  templateUrl: 'dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
 }
