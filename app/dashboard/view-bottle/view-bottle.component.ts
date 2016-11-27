875528777ead9adb96a55f949211d7c1df7dc3a6
Laury Lafage <lowrey97x@hotmail.fr>
2016-11-27 16:56:53 +0100

    Add view bottle.

---
+++ app/dashboard/view-bottle/view-bottle.component.ts
@@ -0,0 +1,9 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'view-bottle',
+  templateUrl: 'view-bottle.component.html'
+})
+export class ViewBottleComponent {
+}
---
+++ app/dashboard/view-bottle/view-bottle.module.ts
@@ -0,0 +1,9 @@
+import { NgModule }             from '@angular/core';
+import { ViewBottleComponent }  from './view-bottle.component';
+
+@NgModule({
+  declarations: [ ViewBottleComponent ],
+  exports: [ ViewBottleComponent ]
+})
+export class ViewBottleModule {
+}
