b797f4eaacff9c396645bdbd20254aae983c99cc
Laury Lafage <lowrey97x@hotmail.fr>
2016-11-27 01:46:10 +0100

    Add clock.

--- app/dashboard/bottle-card/bottle-card.component.ts
+++ app/dashboard/bottle-card/bottle-card.component.ts
@@ -3,7 +3,8 @@ import { Component, Input } from '@angular/core';
 @Component({
   moduleId: module.id,
   selector: 'bottle-card',
-  templateUrl: 'bottle-card.component.html'
+  templateUrl: 'bottle-card.component.html',
+  styleUrls: ['./bottle-card.component.css']
 })
 export class BottleCardComponent {
   @Input() title: string;
