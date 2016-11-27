e524e2d1f23c66c59f713b2f379e5c9309d6858f
Laury Lafage <lowrey97x@hotmail.fr>
2016-11-27 22:14:51 +0100

    Try to find a way...

--- app/dashboard/view-bottle/view-bottle.component.ts
+++ app/dashboard/view-bottle/view-bottle.component.ts
@@ -3,7 +3,8 @@ import { Component, Input } from '@angular/core';
 @Component({
   moduleId: module.id,
   selector: 'view-bottle',
-  templateUrl: 'view-bottle.component.html'
+  templateUrl: 'view-bottle.component.html',
+  styleUrls: ['./view-bottle.component.css']
 })
 export class ViewBottleComponent {
 }
