cd06670d440ed4d64955164bb65f2ae3fba9ef45
Laury Lafage <lowrey97x@hotmail.fr>
2016-11-28 19:36:31 +0100

    Make it work on heroku.

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname));
 app.use(morgan('dev'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'client/index.html'));
 });
 
 app.listen(portNumber, function () {
