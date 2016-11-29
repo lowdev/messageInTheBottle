dce73d1d7d19252293d7414e370326c81b4ac377
Laury Lafage <lowrey97x@hotmail.fr>
2016-11-29 23:30:21 +0100

    Let the routing angular operate :-)

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const express = require('express'),
 app.use(express.static(__dirname)); 
 app.use(morgan('dev'));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'client/index.html'));
+app.all('*', function (req, res) {
+	res.status(200).sendFile(path.join(__dirname, '/index.html')); 
 });
 
 app.listen(portNumber, function () {
