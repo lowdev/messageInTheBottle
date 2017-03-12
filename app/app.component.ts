717e852a41acfeec951eb4bcfff8efc84ecdf10a
Laury Lafage <lowrey97x@hotmail.fr>
2017-03-12 21:16:58 +0100

    Fix map button for tablet format.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
+  selector: 'app',
   template: `<router-outlet></router-outlet>`
 })
 export class AppComponent { }
